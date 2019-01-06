a2278e16975583ded493f5fb21d1fb8a0e0a4734
Tomas <tcarneado@gmail.com>
2019-01-07 00:23:09 +0100

    Init

---
+++ deploy/teamcity_deploy_step_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#*****************************************
+#Teamcity BuildStep: Command Line
+#chmod a+x teamcity_deploy_step_test.sh
+#./teamcity_deploy_step_test
+#*****************************************
+
+docker stack deploy -c test.yml ftpayment_ui_test
---
+++ public/ftpayment-wc.js
@@ -0,0 +1,16 @@
+class FTPaymentDashboard extends HTMLElement {
+ constructor() {
+    super();
+
+    const shadowRoot = this.attachShadow({mode: 'closed'});
+    shadowRoot.innerHTML = `
+            <style>
+                p {
+                    color: red;
+                }
+            </style>
+            <p>FTPayments!</p>
+    `;
+  }
+}
+window.customElements.define("ftpayment-dashboard", FTPaymentDashboard);
